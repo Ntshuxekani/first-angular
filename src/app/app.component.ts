934b13524c0bec5133d46efbf633b2b73c88d249
ntshuxi <ntshuxekanizitha@gmail.com>
2018-07-11 11:52:08 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'LOGIN';
+
+  username:string;
+  ngUname:string;
+
+  display(): void{
+    this.username="welcome to " +this.ngUname+ " page";
+  }
 }
